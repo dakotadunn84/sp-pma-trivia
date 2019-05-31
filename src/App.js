600571dc3b1d0eb65d7f112055f3f7b7be7517bd
Dakota <dakotadunn84@gmail.com>
2019-05-31 00:40:45 -0700

    QuestionCard props and cycle state

--- src/App.js
+++ src/App.js
@@ -11,19 +11,20 @@ class App extends React.Component {
       <div className="App">
         <Container>
           <Row>
-            <Col xs="6" sm="4"><QuestionCard /></Col>
-            <Col xs="6" sm="4"><QuestionCard /></Col>
-            <Col xs="6" sm="4"><QuestionCard /></Col>
+            <Col xs="6" sm="4"><QuestionCard pointValue="100" question="Do apples have seeds?" answer="Yes" /></Col>
+            <Col xs="6" sm="4"><QuestionCard pointValue="100" question="What color are bananas?" answer="Yellow" /></Col>
+            <Col xs="6" sm="4"><QuestionCard pointValue="100" question="What color are carrots?" answer="Orange" /></Col>
           </Row>
           <Row>
-            <Col xs="6" sm="4"><QuestionCard /></Col>
-            <Col xs="6" sm="4"><QuestionCard /></Col>
-            <Col xs="6" sm="4"><QuestionCard /></Col>
+          <Col xs="6" sm="4"><QuestionCard pointValue="200" question="What type of plant do apples grow from?" answer="Tree" /></Col>
+            <Col xs="6" sm="4"><QuestionCard pointValue="200" question="Which end of the banana is the right way to open it?" answer="The bottom" /></Col>
+            <Col xs="6" sm="4"><QuestionCard pointValue="200" question="Do carrots grow from trees?" answer="No" /></Col>
+            
           </Row>
           <Row>
-            <Col xs="6" sm="4"><QuestionCard /></Col>
-            <Col xs="6" sm="4"><QuestionCard /></Col>
-            <Col xs="6" sm="4"><QuestionCard /></Col>
+          <Col xs="6" sm="4"><QuestionCard pointValue="300" question="Does an apple a day keep the doctor away?" answer="Yes" /></Col>
+          <Col xs="6" sm="4"><QuestionCard pointValue="300" question="How many bananas does Roger Harrington eat in a day?" answer="15" /></Col>
+            <Col xs="6" sm="4"><QuestionCard pointValue="300" question="Does Bugs Bunny love carrots?" answer="Yes" /></Col>
           </Row>
         </Container>
       </div>
--- src/components/QuestionCard.js
+++ src/components/QuestionCard.js
@@ -1,19 +1,67 @@
 import React from 'react';
-import { Card, CardFooter, CardBody,CardTitle, CardText } from 'reactstrap';
+import { Card, CardFooter, CardHeader, CardBody, CardText } from 'reactstrap';
 
 
 class QuestionCard extends React.Component {
+    state = {status: 'start'};
+
+    onInputClick = () => {
+
+        if(this.state.status==='start'){
+            this.setState({status:'question'})
+        }
+
+        if(this.state.status==='question'){
+            this.setState({status:'done'})
+        }
+
+        // Optional QuestionCard reset
+        // if(this.state.status==='done'){
+            
+        //     this.setState({status:'start'})
+        // }
+    };
+
+    renderContent() {
+        if (this.state.status === "start") {
+            return (
+                <Card className="text-center">
+                    <CardBody>
+                        <CardText>{this.props.pointValue}</CardText>
+                    </CardBody>
+                </Card>
+            );
+        }
+
+        if (this.state.status === "question") {
+            return (
+                <Card className="text-center">
+                    <CardBody>
+                        <CardText>{this.props.question}</CardText>
+                    </CardBody>
+                </Card>
+            );
+        }
+        
+        if (this.state.status === "done") {
+            return (
+                <Card className="text-center">
+                    <CardHeader>{this.props.pointValue}</CardHeader>
+                    <CardBody>
+                        <CardText>{this.props.question}</CardText>
+                    </CardBody>
+                    <CardFooter className="text-muted">{this.props.answer}</CardFooter>
+                </Card>
+            );
+        }
+
+
+    }
 
     render() {
         return (
-            <div>
-                <Card className="text-center">
-                    <CardBody>
-                        <CardTitle>Point Total</CardTitle>
-                        <CardText>Question question question question question question question question question</CardText>
-                    </CardBody>
-                    <CardFooter className="text-muted">Answer</CardFooter>
-                </Card>
+            <div onClick={this.onInputClick}>
+                {this.renderContent()}
             </div>
         );
     }
