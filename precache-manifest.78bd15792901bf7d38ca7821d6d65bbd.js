706689e16c37118ef7a1d78b1b569541962cfbf9
Dakota Dunn <dakotadunn84@gmail.com>
2019-07-21 18:33:55 -0700

    Updates

---
+++ precache-manifest.78bd15792901bf7d38ca7821d6d65bbd.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b00aadd82d59246b80e4e82ed6925413",
+    "url": "/sp-pma-trivia/index.html"
+  },
+  {
+    "revision": "bcf8b8de32831b53f6f6",
+    "url": "/sp-pma-trivia/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "a2d57b21d3e7d25553d7",
+    "url": "/sp-pma-trivia/static/css/main.ee3eda7b.chunk.css"
+  },
+  {
+    "revision": "bcf8b8de32831b53f6f6",
+    "url": "/sp-pma-trivia/static/js/2.afd1978b.chunk.js"
+  },
+  {
+    "revision": "a2d57b21d3e7d25553d7",
+    "url": "/sp-pma-trivia/static/js/main.fe914056.chunk.js"
+  },
+  {
+    "revision": "0928fa4584b18dc08625",
+    "url": "/sp-pma-trivia/static/js/runtime~main.7eb5251a.js"
+  },
+  {
+    "revision": "b8ed3d26fccdf97c7f4c92b10bccf7b3",
+    "url": "/sp-pma-trivia/static/media/Apples.b8ed3d26.png"
+  },
+  {
+    "revision": "184726b9c572ffa44731fc40914808ac",
+    "url": "/sp-pma-trivia/static/media/Grapes.184726b9.png"
+  },
+  {
+    "revision": "d53da5431aeab98a8720d7252d2b8fee",
+    "url": "/sp-pma-trivia/static/media/Japan.d53da543.png"
+  },
+  {
+    "revision": "99751c72af7362f43cda9d76a501dcf4",
+    "url": "/sp-pma-trivia/static/media/Latin & Baja.99751c72.png"
+  },
+  {
+    "revision": "7ddc0f7ee713f11f084060518de131cd",
+    "url": "/sp-pma-trivia/static/media/Pears.7ddc0f7e.png"
+  },
+  {
+    "revision": "cd1eaed9befd38f8f02d0f91999a0563",
+    "url": "/sp-pma-trivia/static/media/Stonefruit.cd1eaed9.png"
+  }
+]);
