28c23dc82d8619c0bd1f661a34fbf111651b1e48
Dakota Dunn <dakotadunn84@gmail.com>
2019-09-04 18:28:24 -0700

    Updates

--- precache-manifest.1822d3eefebc99a24c73e2787964dd90.js
+++ precache-manifest.1822d3eefebc99a24c73e2787964dd90.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "539fb5b53f3813832d219653bb5b6554",
+    "revision": "e5ec24c6a93633c48101612cb185a842",
     "url": "/sp-pma-trivia/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/sp-pma-trivia/static/css/2.266e55a5.chunk.css"
   },
   {
-    "revision": "684ff51583305e1771ab",
-    "url": "/sp-pma-trivia/static/css/main.8286baab.chunk.css"
+    "revision": "3d627f4cbe547f824dd9",
+    "url": "/sp-pma-trivia/static/css/main.3e6ede52.chunk.css"
   },
   {
     "revision": "89ad950f87dc0b58e611",
     "url": "/sp-pma-trivia/static/js/2.719e9f5c.chunk.js"
   },
   {
-    "revision": "684ff51583305e1771ab",
-    "url": "/sp-pma-trivia/static/js/main.d12445df.chunk.js"
+    "revision": "3d627f4cbe547f824dd9",
+    "url": "/sp-pma-trivia/static/js/main.7032f913.chunk.js"
   },
   {
     "revision": "0928fa4584b18dc08625",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sp-pma-trivia/precache-manifest.7cb1a47f5835995d9061da8c54e94aa5.js"
+  "/sp-pma-trivia/precache-manifest.1822d3eefebc99a24c73e2787964dd90.js"
 );
 
 self.addEventListener('message', (event) => {
